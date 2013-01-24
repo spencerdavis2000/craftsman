dd70ab870ed57bd09ef577abe405c0415fe8683e
Spencer Davis <spencerdavis2000@gmail.com>
2013-01-24 10:57:48 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#practice project for Spencer Davis by Li-Hsuan (mentor)
+
+This is a practice rails project for Spencer Davis by mentor Li-Hsuan at [8th Light](http://www.8thLight.com/)
+
+It is a application to track the progress of a craftsman on each project as well and eventually be able
+
+to connect the craftsmen together so the progress is open to everyone.  
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craftsman::Application.config.secret_token = '805a4beb791d5c7e606e5ba629cc0db927eeee79ab66f438b1a8eeb0a956a5bc9dc347bb950516ef5b9e56d2fc88cc8aa23d1e8f39192b91259a8cbce7b1ca2f'
